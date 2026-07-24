16098e835210b68c5be8a9e8229dad26bb4332dd
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2026-07-24 09:59:16 +0000

    Commit

---
+++ ui-project/src/Components/Section1/PageContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LeftContent from './LeftContent.jsx'
+import RightContent from './RightContent.jsx'
+
+const PageContent = () => {
+  return (
+    <div className='py-3 px-18'>
+      <LeftContent />
+      <RightContent />
+      
+    </div>
+  )
+}
+
+export default PageContent
+
