c851cef748bee1fdfd3719f08f1fdb1d22e9e2c6
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2026-07-25 17:11:39 +0000

    July-25

---
+++ ui-project/src/Components/Section1/LeftContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { RiArrowRightUpLongLine } from "@remixicon/react";
+const LeftContent = () => {
+  return (
+    <div className='h-full w-1/4 bg-blue-600'>
+      <h1>Lefty Content</h1>
+      <div>
+        <h3 className='text-4xl px-2 py-8 mt-18'>Prospective Customer Segmentation</h3>
+      </div>
+       <div className=' mt-auto text-8xl'>
+
+           <RiArrowRightUpLongLine size='40px'/>
+        </div>
+    </div>
+  )
+}
+
+export default LeftContent
---
+++ ui-project/src/Components/Section1/RightContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightContent = () => {
+  return (
+    <div className='h-full w-3/4  bg-orange-600'>
+      <h1>Right Content</h1>
+    </div>
+  )
+}
+
+export default RightContent
