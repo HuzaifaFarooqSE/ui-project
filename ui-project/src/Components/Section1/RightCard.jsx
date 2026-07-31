dbab5726ea0aca4bc0b05d59d1186fba400d33f5
HuzaifaFarooqSE <malikhuzaifa1309@gmail.com>
2026-07-31 03:18:48 +0000

    Maybe the last commit of UI Project

--- ui-project/src/Components/Section1/RightCard.jsx
+++ ui-project/src/Components/Section1/RightCard.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const RightCard = () => {
   return (
-    <div>
-      Right card
+    <div className='h-full relative rounded-3xl bg-red-500 w-70 shrink-0 overflow-hidden'>
+      <img className='h-full w-full object-cover ' src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+      <div className='absolute top-0 h-full w-full   p-6 flex flex-col justify-between'>
+          <h2 className='bg-white rounded-full h-10 w-10 flex justify-center items-center'>1</h2>
+          <div >
+            <p className='mb-5 text-lg text-white leading-normal'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus distinctio vitae ea rerum mollitia libero </p>
+            <div>
+              <button className='bg-blue-500 p-3 rounded-4xl'>Satisfy</button>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
--- ui-project/src/Components/Section1/RightContent.jsx
+++ ui-project/src/Components/Section1/RightContent.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import RightCard from './RightCard.jsx'
 const RightContent = () => {
   return (
-    <div className='h-full w-3/4  bg-orange-600'>
-      <h1>Right Content</h1>
+    <div className='h-full w-2/3 p-4 flex overflow-x-auto flex-nowrap gap-10 rounded-2xl'>
+      
+      <RightCard />
+      <RightCard />
+      <RightCard />
+      <RightCard />
+      <RightCard />
       <RightCard />
     </div>
   )
